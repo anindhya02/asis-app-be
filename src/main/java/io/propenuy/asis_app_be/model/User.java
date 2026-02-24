46dad21462d317d37dd84acbcd40615d2edc7f56
anindhyaputri.paramitha <anindhyaputri.paramitha@ui.ac.id>
2026-02-24 17:40:06 +0700

    Environment Setup

---
+++ src/main/java/io/propenuy/asis_app_be/model/User.java
@@ -0,0 +1,36 @@
+package io.propenuy.asis_app_be.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nama;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
+
+enum Role {
+    ADMIN,
+    PENGURUS,
+    KETUA_YAYASAN
+}
