56213d71d5e71f4b9b158e17074b7aaf6e53c250
anindhyaputri.paramitha <anindhyaputri.paramitha@ui.ac.id>
2026-05-27 23:19:12 +0700

    fix: update WebSecurityConfig for deployment

--- src/main/java/io/propenuy/asis_app_be/security/WebSecurityConfig.java
+++ src/main/java/io/propenuy/asis_app_be/security/WebSecurityConfig.java
@@ -128,7 +128,7 @@ public class WebSecurityConfig {
         config.setAllowedOrigins(List.of(
             "http://localhost:5173",
             "http://localhost:5174",
-            "https://asis-app-fe-2.vercel.app"
+            "https://ash-sholihati.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
